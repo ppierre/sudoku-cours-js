2bd6e3b582102deeeb9c8eb4472bc120f98261c4
Pierre Pracht <pierre.pracht@gmail.com>
2009-02-04 20:35:07 +0100

    Ajout d'exemple pour les tests

--- sequence-5-Classe_SudokuGrid/javascript/code-tests-simples.js
+++ sequence-5-Classe_SudokuGrid/javascript/code-tests-simples.js
@@ -1,5 +1,21 @@
 // = Tests unitaires : SudokuGrid (fonctions simples)
 
+// ** Exemple de tests : simple assertion
+
+fireunit.ok(2 * 2 === 4, "deux fois deux font quatre");
+
+// ** Exemple de tests : comparaison
+//
+// cliquer sur le test puis utiliser l'onglet compare pour le corriger
+
+tableau = [1, 3, 2];
+
+tableau_double = [1, 3, 2, 1, 3, 2];
+
+fireunit.compare(
+  tableau_double.toSource(),
+  tableau.extend([1, 2, 3]).toSource(),
+  "1, 3, 2 suivit de 1, 3, 2 donne 1, 3, 2, 1 ,3 ,2");
 
 // ** Tests de {{{extract_cols}}}
 //
@@ -18,3 +34,5 @@
 // À écrire !
 
 
+// ** Fin des tests
+fireunit.testDone();
